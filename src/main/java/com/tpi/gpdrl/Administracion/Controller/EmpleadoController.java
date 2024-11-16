9dd627badbf73a5dd1ecf2cb48f73b601e55c103
Manuel <aa21050@ues.edu.sv>
2024-11-15 23:44:53 -0600

    cambios

--- src/main/java/com/tpi/gpdrl/Administracion/Controller/EmpleadoController.java
+++ src/main/java/com/tpi/gpdrl/Administracion/Controller/EmpleadoController.java
@@ -1,15 +1,18 @@
 package com.tpi.gpdrl.Administracion.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("Empleados")
 public class EmpleadoController {
-    
+
     @GetMapping("/listadoEmpleado")
-    public String verEmpleado(){
+    public String verEmpleado(Model model) {
+
+        model.addAttribute("titulo", "Listado empleado");
         return "Empleado/listadoEmpleado";
     }
 }
